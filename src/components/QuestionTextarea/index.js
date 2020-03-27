ce6e8a09c97620845e4999fc31b231acca189e5b
WingLim <643089849@qq.com>
2020-03-27 12:07:23 +0800

    feat: use onChange to save performance

--- src/components/QuestionTextarea/index.js
+++ src/components/QuestionTextarea/index.js
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 
-const QuestionTextarea = ({ onInput, id, value, label }) => (
+const QuestionTextarea = ({ onChange, id, value, label }) => (
   <div key={id} class="question">
     <label for={id} dir="auto">
       {label}
@@ -10,11 +10,11 @@ const QuestionTextarea = ({ onInput, id, value, label }) => (
       dir="auto"
       value={value}
       placeholder="Start writing..."
-      onInput={event => {
+      onChange={event => {
         event.target.style.height = 'auto';
         event.target.getBoundingClientRect();
         event.target.style.height = event.target.scrollHeight + 'px';
-        onInput(event.target.value);
+        onChange(event.target.value);
       }}
     />
   </div>
